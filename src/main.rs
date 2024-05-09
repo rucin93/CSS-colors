2e22aab5df9ab4d8b7285c705390ae82c01d07d4
prucinski <prucinski@filmweb.pl>
2024-05-10 00:23:18 +0200

    Add fixed size vector instead of push

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ use rug::{ops::Pow, Float};
 
 
 //// PARAMS - START
-const MAX_CACHE_SIZE: usize = 16 * 10i32.pow(4) as usize;
+const MAX_CACHE_SIZE: usize = 16 * 10i32.pow(5) as usize;
 // const MAX_CACHE_SIZE: usize = 16 * 10i32.pow(6) as usize;
-const INDEX: i32 = 8;
+const INDEX: i32 = 12;
 const BASE_16: &[char; 16] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 ];
 
 const CHAR_RANGE: std::ops::Range<u16> = 1..0x7FF;
-const PRECISION: u32 = 80;
+const PRECISION: u32 = 100;
 
-const HASH_FUNCTION: &str = "e**.1/";
+const HASH_FUNCTION: &str = "e**.1*";
 
 fn hash(store: Float, value: u32) -> Float {
     // return store / Float::with_val(PRECISION, value);
-    return store.pow(0.1) / Float::with_val(PRECISION, value);
+    return store.pow(0.1) * Float::with_val(PRECISION, value);
 }
 
 //// PARAMS - end
@@ -72,7 +72,6 @@ impl Encoder {
 
         let clone = initial_state.clone();
 
-
         Encoder {
             initial_state,
             target_hex,
@@ -85,8 +84,11 @@ impl Encoder {
     }
 
     fn generate_next_states(&self, state: &State) -> Vec<State> {
-        let mut next_states = Vec::with_capacity(MAX_CACHE_SIZE * 2);
-        for char in &self.possible_chars {
+        // create next_states vector with fixed length of possible_chars with no default value
+        let mut next_states = vec![State::new(Float::with_val(PRECISION, 0.0), 0, 0, vec![]); self.possible_chars.len()];
+        
+        let mut counter = 0;
+        for char in self.possible_chars.iter(){
             let current_pair = &self.target_hex_pairs[state.current_pair_index];
             let current_hash = create_hash(state.start.clone() as Float, *char as u32, current_pair.len());
 
@@ -107,7 +109,8 @@ impl Encoder {
 
                 // only add when byte count is less than 510 - prune too big strings from output
                 if new_state.byte_count < 510 { 
-                    next_states.push(new_state);
+                    next_states[counter] = new_state;
+                    counter += 1;
                 }
 
                 // if next_states.len() > 10 {
@@ -130,35 +133,24 @@ impl Encoder {
         }
     }
 
-    fn encode(&mut self) -> Option<State> {
+    fn encode(&mut self) -> Option<Vec<State>> {
         let mut completed = false;
         let mut counter = 0;
         while !completed {
-
-            // do it all in parallel
-
-            // for state in &self.old_cache {
-            //     let next_states = self.generate_next_states(state);
-
-            //     for s in next_states {
-            //         self.new_cache.push(s);
-            //     }
-            // }
-
             use rayon::prelude::*;
 
             self.new_cache = self.old_cache.par_iter().flat_map(|state| {
                 self.generate_next_states(state)
-            }).collect();
+            }).filter(|state| state.byte_count > 0).collect();
 
             counter += 1;
             println!("{} {} of {} - {}", chrono::Utc::now().format("%d/%m/%Y %H:%M:%S"), counter, self.target_hex_pairs.len(), self.new_cache.len());
 
             self.prune_cache();
 
-            if counter >= self.target_hex_pairs.len() {
+            if counter == self.target_hex_pairs.len() {
                 completed = true;
-                return Some(self.new_cache[0].clone());
+                return Some(self.new_cache.clone());
             }
 
             self.old_cache = self.new_cache.clone();
@@ -183,7 +175,8 @@ fn is_valid_char(x: i32) -> bool {
     // 96 - `
     // 127 - delete
     match x {
-        13 | 36 | 92 | 96 | 127 => false,
+        13 | 92 | 96 | 127 => false,
+        // 13 | 36 | 92 | 96 | 127 => false,
         _ => true,
     }
 }
@@ -222,12 +215,9 @@ fn get_hex_digit(x: Float, d: usize) -> char {
 
 // create two hash values for each pair to check if the condition is satisfied
 fn create_hash(start: Float, x: u32, size: usize) -> Vec<Float> {
-    let start_copy = start.clone();
-    let mut hash_values: Vec<Float> = vec![hash(start_copy, x)];
-    
-    let new_hash = hash(start + hash_values[0].clone(), x);
-    hash_values.push(new_hash);
-    hash_values
+    let h1 = hash(start.clone(), x);
+    let new_hash = hash(start + h1.clone(), x);
+    return vec![h1, new_hash];
 }
 
 fn check_condition(
@@ -256,7 +246,7 @@ fn check_condition(
 }
 
 fn main() {
-    // let target_hex = "f0f8fffaebd700".to_string();
+    // let target_hex = "0f8fffae".to_string();
     let target_hex = "f0f8fffaebd700ffff7fffd4f0fffff5f5dcffe4c4000000ffebcd0000ff8a2be2a52a2adeb8875f9ea07fff00d2691eff7f506495edfff8dcdc143c00ffff00008b008b8bb8860ba9a9a9006400a9a9a9bdb76b8b008b556b2fff8c009932cc8b0000e9967a8fbc8f483d8b2f4f4f2f4f4f00ced19400d3ff149300bfff6969696969691e90ffb22222fffaf0228b22ff00ffdcdcdcf8f8ffffd700daa520808080008000adff2f808080f0fff0ff69b4cd5c5c4b0082fffff0f0e68ce6e6fafff0f57cfc00fffacdadd8e6f08080e0fffffafad2d3d3d390ee90d3d3d3ffb6c1ffa07a20b2aa87cefa778899778899b0c4deffffe000ff0032cd32faf0e6ff00ff80000066cdaa0000cdba55d39370db3cb3717b68ee00fa9a48d1ccc71585191970f5fffaffe4e1ffe4b5ffdead000080fdf5e68080006b8e23ffa500ff4500da70d6eee8aa98fb98afeeeedb7093ffefd5ffdab9cd853fffc0cbdda0ddb0e0e6800080663399ff0000bc8f8f4169e18b4513fa8072f4a4602e8b57fff5eea0522dc0c0c087ceeb6a5acd708090708090fffafa00ff7f4682b4d2b48c008080d8bfd8ff634740e0d0ee82eef5deb3fffffff5f5f5ffff009acd3".to_string();
     // reverse target_hex
     let target_hex = target_hex.chars().rev().collect::<String>();
@@ -268,18 +258,25 @@ fn main() {
 
     if let Some(result) = encoder.encode() {
         println!("Encoding Complete:");
-        println!("Encoded Sequence: {}", result.history.iter().collect::<String>());
-        println!("Byte Count: {}", result.byte_count);
-        // let timestamp = chrono::Utc::now().timestamp();
-        fs::write(
-            format!("out/rs.txt"),
-            format!(
-                "for(w=i=e=2;e+={HASH_FUNCTION}`-{}`.charCodeAt(i++/2);)w=e.toString(16)[{}]+w",
-                result.history.iter().collect::<String>(),
-                INDEX
-            ),
-        )
-        .expect("Failed to write to file");
+        // group result by byte count
+        let mut result = result.into_iter().collect::<Vec<_>>();
+        result.sort_by(|a, b| a.byte_count.cmp(&b.byte_count));
+        // write to file first 15 results
+        let result = result.into_iter().take(15).collect::<Vec<_>>();
+
+        for (i, r) in result.iter().enumerate() {
+            println!("Encoded Sequence {}: {}", i, r.history.iter().collect::<String>());
+            println!("Byte Count {}: {}", i, r.byte_count);
+            fs::write(
+                format!("out/rs_{}.txt", i),
+                format!(
+                    "for(w=i=e=2;e+={HASH_FUNCTION}`-{}`.charCodeAt(i++/2);)w=e.toString(16)[{}]+w\n\n",
+                    r.history.iter().collect::<String>(),
+                    INDEX
+                ),
+            )
+            .expect("Failed to write to file");
+        }
     } else {
         println!("No valid encoding found.");
     }
