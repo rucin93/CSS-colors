4e337e5f43f5b92954868a39754f2071dba86c2d
prucinski <prucinski@filmweb.pl>
2024-05-10 16:13:25 +0200

    Possible chars out of class instance

--- test.js
+++ test.js
@@ -74,19 +74,11 @@ function get_hex_digit(x, d)
       this.cacheSize = cacheSize;
       this.oldCache = new Set([initialState]); // Initialize with the initial state
       this.newCache = new Set();
-
-      this.possibleChars = [];
-      // Generate all valid characters
-      for (let char = 1; char < 0xfff; char++) {
-        if (isValidChar(char)) {
-            this.possibleChars.push(String.fromCodePoint(char));
-        }
-      }
     }
   
     generateNextStates(state) {      
       const nextStates = [];
-      for (const char of this.possibleChars) {
+      for (const char of possibleChars) {
         const currentPair = this.targetHexPairs[state.currentPairIndex]; // Current hex pair
         const currentHash = createHash(state.start, char.codePointAt(), currentPair.length);
         const conditionMet = checkCondition(
@@ -160,13 +152,13 @@ function get_hex_digit(x, d)
   // Example Usage
 //   const targetHex = "0f8fffae"; // Target hex string
   const targetHex = "0f8fffaebd700ffff7fffd4f0fffff5f5dcffe4c4000000ffebcd0000ff8a2be2a52a2adeb8875f9ea07fff00d2691eff7f506495edfff8dcdc143c00ffff00008b008b8bb8860ba9a9a9006400a9a9a9bdb76b8b008b556b2fff8c009932cc8b0000e9967a8fbc8f483d8b2f4f4f2f4f4f00ced19400d3ff149300bfff6969696969691e90ffb22222fffaf0228b22ff00ffdcdcdcf8f8ffffd700daa520808080008000adff2f808080f0fff0ff69b4cd5c5c4b0082fffff0f0e68ce6e6fafff0f57cfc00fffacdadd8e6f08080e0fffffafad2d3d3d390ee90d3d3d3ffb6c1ffa07a20b2aa87cefa778899778899b0c4deffffe000ff0032cd32faf0e6ff00ff80000066cdaa0000cdba55d39370db3cb3717b68ee00fa9a48d1ccc71585191970f5fffaffe4e1ffe4b5ffdead000080fdf5e68080006b8e23ffa500ff4500da70d6eee8aa98fb98afeeeedb7093ffefd5ffdab9cd853fffc0cbdda0ddb0e0e6800080663399ff0000bc8f8f4169e18b4513fa8072f4a4602e8b57fff5eea0522dc0c0c087ceeb6a5acd708090708090fffafa00ff7f4682b4d2b48c008080d8bfd8ff634740e0d0ee82eef5deb3fffffff5f5f5ffff009acd32"; // Target hex string
-  const targetHexPairs = targetHex.match(/.{1,2}/g); // Create pairs from the hex string
+  const targetHexPairs = targetHex.split``.reverse().join``.match(/.{1,2}/g); // Create pairs from the hex string
   console.log(targetHexPairs)
 
   const initialState = new State({
     start: 2, currentPairIndex: 0, byteCount: 0, history: []
   }); 
-  const encoder = new Encoder(initialState, targetHex, targetHexPairs, 100); // Cache size of 1000
+  const encoder = new Encoder(initialState, targetHex, targetHexPairs, 16*10); // Cache size of 1000
   
   const result = encoder.encode();
   
@@ -174,7 +166,7 @@ function get_hex_digit(x, d)
     console.log("Encoding Complete:");
     console.log("Encoded Sequence:", result.history.join(""));
     console.log("Byte Count:", result.byteCount);
-    fs.writeFile(new Date().getTime() + '.txt', `for(w='f',i=e=2;e+=e**.2*\`-${result.history.join("")}\`.codePointAt(i++/2);)w+=e.toString(16)[${INDEX}]`, (err) => {
+    fs.writeFile("out/" + 'js' + '.txt', `for(w=i=e=2;e+=e**.1*\`-${result.history.join("")}\`.codePointAt(i++/2);)w=e.toString(16)[${INDEX}]+w`, (err) => {
         if (err) {
             console.log(err)
         }
