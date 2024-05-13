380ac88b1d40cd7373afc5ffed845755fad48c49
prucinski <prucinski@filmweb.pl>
2024-05-13 22:46:51 +0200

    Change Float to f64

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 use std::{fs};
 use chrono;
-use rug::{ops::Pow, Float};
+// use rug::{ops::Pow, Float};
 use std::sync::Mutex;
 // use lazy_static::lazy_static;
 
 //// PARAMS - START
-const MAX_CACHE_SIZE: usize = 16 * 10i32.pow(5) as usize;
+const MAX_CACHE_SIZE: usize = 16 * 10i32.pow(6) as usize;
 // const MAX_CACHE_SIZE: usize = 16 * 10i32.pow(6) as usize;
-const INDEX: i32 = 8;
+const INDEX: i32 = 13;
 const BASE_16: &[char; 16] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 ];
 
 const CHAR_RANGE: std::ops::Range<u32> = 1..0x7FF;
-const PRECISION: u32 = 100;
+// const PRECISION: u32 = 100;
 
-const HASH_FUNCTION: &str = "e/";
+const HASH_FUNCTION: &str = "e/i/";
 
-fn hash(store: Float, value: u32) -> Float {
+fn hash(store: f64, value: u32, index: u32) -> f64 {
     // return store / Float::with_val(PRECISION, value);
-    return store / Float::with_val(PRECISION, value);
+    return store / value as f64 ;
 }
 
 //// PARAMS - end
@@ -47,6 +47,11 @@ impl Cache {
         }
     }
 
+    fn get(&self) -> usize {
+        let current_size = self.size.lock().unwrap();
+        *current_size
+    }
+
     fn prune(&self) {
         let mut current_size = self.size.lock().unwrap();
         *current_size = 0;
@@ -57,7 +62,7 @@ impl Cache {
 
 #[derive(Clone, PartialEq)]
 struct State {
-    start: Float,
+    start: f64,
     current_pair_index: usize,
     byte_count: usize,
     history: Vec<char>,
@@ -65,7 +70,7 @@ struct State {
 }
 
 impl State {
-    fn new(start: Float, current_pair_index: usize, byte_count: usize, history: Vec<char>, last_char: char) -> Self {
+    fn new(start: f64, current_pair_index: usize, byte_count: usize, history: Vec<char>, last_char: char) -> Self {
         State {
             start,
             current_pair_index,
@@ -117,11 +122,13 @@ impl Encoder {
     
             // Add to group 1 (all characters)
             group1.push(ch);
+            group2.push(ch);
+
     
-            // Add to group 2 (byte size 1 and 2)
-            if byte_size == 1 || byte_size == 2 {
-                group2.push(ch);
-            }
+            // // Add to group 2 (byte size 1 and 2)
+            // if byte_size == 1 || byte_size == 2 {
+            //     group2.push(ch);
+            // }
     
             // Add to group 3 (byte size 1)
             if byte_size == 1 {
@@ -151,7 +158,7 @@ impl Encoder {
         }
 
         for &char in self.char_groups[state.byte_count - best_size].iter() {
-            let current_hash = create_hash(state.start.clone(), char as u32);
+            let current_hash = create_hash(state.start.clone(), char as u32, index as u32);
             let clone = current_hash.clone();
             // Checking conditions before any heavy operations like cloning
             if check_condition(state.start.clone(), current_hash, &current_pair, INDEX as u32) {
@@ -174,7 +181,7 @@ impl Encoder {
             }
 
             // check if self.new_cache is full
-            if next_states.len() >= MAX_CACHE_SIZE/8 {
+            if next_states.len() >= MAX_CACHE_SIZE {
                 return next_states;
             }
         }
@@ -204,7 +211,7 @@ impl Encoder {
             let best_size = sorted_states[0].byte_count;
 
             self.new_cache = self.old_cache.par_iter().enumerate().flat_map(|(index, state)| {
-                self.generate_next_states(state, index, best_size) // Pass index to generate_next_states
+                self.generate_next_states(state, counter + 2, best_size) // Pass index to generate_next_states
             }).collect();
 
             counter += 1;
@@ -248,30 +255,30 @@ fn is_valid_char(x: i32) -> bool {
 }
 
 // get hex digit at dth position
-fn get_hex_digit(x: Float, d: usize) -> char {
+fn get_hex_digit(x: f64, d: usize) -> char {
 
     let x = x.clone();
-    let d = Float::with_val(PRECISION, d);
+    let d = d as f64;
 
-    let mut shift = Float::with_val(PRECISION, 1.0);
-    let mut threshold = Float::with_val(PRECISION, 16.0);
+    let mut shift = 1.0;
+    let mut threshold = 16.0;
 
     while x >= threshold {
-        shift += Float::with_val(PRECISION, 1.0);
-        threshold *= Float::with_val(PRECISION, 16.0);
+        shift += 1.0;
+        threshold *= 16.0;
     }
 
     if d == shift {
         return 'z';
     } else if d < shift {
-        let digit = ((x.to_f32() as i32) >> (((shift - Float::with_val(PRECISION, 1.0) - d).to_f32() as i32) * Float::with_val(PRECISION, 4.0).to_f32() as i32)) & 15;
+        let digit = ((x as i64) >> (((shift - 1.0 - d) as i64) * 4.0 as i64)) & 15;
         return BASE_16[digit as usize] as char;
     } else {
-        let adj_x = x * Float::with_val(PRECISION, 16.0).pow((d - shift));
-        let ret = ((adj_x.to_f64() as usize) & 15) as usize;
+        let adj_x = x * (16.0_f64).powf((d - shift));
+        let ret = ((adj_x as usize) & 15) as usize;
 
 
-        if (ret == 0) && (adj_x%Float::with_val(PRECISION, 1.0) == Float::with_val(PRECISION, 0.0)) {
+        if (ret == 0) && (adj_x%1.0 == 0.0) {
             return 'z';
         } else {
             return BASE_16[ret as usize] as char;
@@ -280,15 +287,15 @@ fn get_hex_digit(x: Float, d: usize) -> char {
 }
 
 // create two hash values for each pair to check if the condition is satisfied
-fn create_hash(start: Float, x: u32) -> (Float, Float){
-    let h1 = hash(start.clone(), x);
-    let new_hash = hash(start + h1.clone(), x);
+fn create_hash(start: f64, x: u32, index: u32) -> (f64, f64){
+    let h1 = hash(start.clone(), x, index);
+    let new_hash = hash(start + h1.clone(), x, index);
     return (h1, new_hash);
 }
 
 fn check_condition(
-    start: Float,
-    hash_values: (Float, Float),
+    start: f64,
+    hash_values: (f64, f64),
     pair: &&Vec<char>,
     y: u32
 ) -> bool {
@@ -310,28 +317,6 @@ fn check_condition(
     true
 }
 
-fn get_group(index: usize, total: usize, state: State) -> u32 {
-
-    let max_byte_count = state.current_pair_index * 3;
-    // let (first_limit, second_limit) = match total % 3 {
-    //     0 => (total / 3, 2 * total / 3),
-    //     1 => (total / 3 + 1, 2 * total / 3 + 1),
-    //     _ => (total / 3 + 1, 2 * total / 3 + 2),
-    // };
-
-    let (first_limit, second_limit) = (max_byte_count / 5, 3 * max_byte_count / 5);
-
-    if index < 4 {
-        return 0;
-    }
-
-    match state.byte_count {
-        i if i < first_limit => 0,
-        i if i < second_limit => 1,
-        _ => 2,
-    }
-}
-
 fn main() {
     // let target_hex = "0f8fffaebd".to_string();
     let target_hex = "f0f8fffaebd700ffff7fffd4f0fffff5f5dcffe4c4000000ffebcd0000ff8a2be2a52a2adeb8875f9ea07fff00d2691eff7f506495edfff8dcdc143c00ffff00008b008b8bb8860ba9a9a9006400a9a9a9bdb76b8b008b556b2fff8c009932cc8b0000e9967a8fbc8f483d8b2f4f4f2f4f4f00ced19400d3ff149300bfff6969696969691e90ffb22222fffaf0228b22ff00ffdcdcdcf8f8ffffd700daa520808080008000adff2f808080f0fff0ff69b4cd5c5c4b0082fffff0f0e68ce6e6fafff0f57cfc00fffacdadd8e6f08080e0fffffafad2d3d3d390ee90d3d3d3ffb6c1ffa07a20b2aa87cefa778899778899b0c4deffffe000ff0032cd32faf0e6ff00ff80000066cdaa0000cdba55d39370db3cb3717b68ee00fa9a48d1ccc71585191970f5fffaffe4e1ffe4b5ffdead000080fdf5e68080006b8e23ffa500ff4500da70d6eee8aa98fb98afeeeedb7093ffefd5ffdab9cd853fffc0cbdda0ddb0e0e6800080663399ff0000bc8f8f4169e18b4513fa8072f4a4602e8b57fff5eea0522dc0c0c087ceeb6a5acd708090708090fffafa00ff7f4682b4d2b48c008080d8bfd8ff634740e0d0ee82eef5deb3fffffff5f5f5ffff009acd32".to_string();
@@ -341,7 +326,7 @@ fn main() {
     // split target_hex into pairs of 2 characters which can be indexed 0 and 1
     let target_hex_pairs = target_hex.chars().collect::<Vec<_>>().chunks(2).map(|pair| pair.to_vec()).collect::<Vec<_>>();
 
-    let initial_state = State::new(Float::with_val(PRECISION, 2.0), 0, 0, vec![], ' ');
+    let initial_state = State::new(2.0, 0, 0, vec![], ' ');
     let mut encoder = Encoder::new(initial_state, target_hex_pairs, MAX_CACHE_SIZE);
 
     if let Some(result) = encoder.encode() {
@@ -353,12 +338,12 @@ fn main() {
         let result = result.into_iter().take(15).collect::<Vec<_>>();
 
         for (i, r) in result.iter().enumerate() {
-            println!("Encoded Sequence {}: {}", i, r.history.iter().collect::<String>());
+            // println!("Encoded Sequence {}: {}", i, r.history.iter().collect::<String>());
             println!("Byte Count {}: {}", i, r.byte_count);
             fs::write(
                 format!("out/rs_{}.txt", i),
                 format!(
-                    "for(w=i=e=2;e+={HASH_FUNCTION}`-{}`.charCodeAt(i++/2);)w=e.toString(16)[{}]+w\n\n",
+                    "for(w=i=e=2;e+={HASH_FUNCTION}`-{}`.charCodeAt(i++/2);)w=e.toString(16)[{}]+w",
                     r.history.iter().collect::<String>(),
                     INDEX
                 ),
